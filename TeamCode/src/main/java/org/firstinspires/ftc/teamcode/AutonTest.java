0d4fa4e2a340a125167685d0d86822a67358e7b9
Adi Pulluru <pranav.pulluru15@gmail.com>
2024-09-14 14:52:39 -0500

    CV setup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+import org.firstinspires.ftc.teamcode.bluePipeline;
+
+
+@Autonomous
+public class AutonTest extends OpMode {
+
+    OpenCvWebcam webcam1 = null;
+    bluePipeline bluePipeline = new bluePipeline();
+
+    @Override
+    public void init(){
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webcam1");
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam1 = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+
+        webcam1.setPipeline(bluePipeline);
+
+
+
+    }
+
+    @Override
+    public void loop(){
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bluePipeline.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.firstinspires.ftc.ftccommon.external.OnCreate;
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+public class bluePipeline extends OpenCvPipeline {
+    public Mat processFrame(Mat input){
+        return input;
+    }
+}
